c9b141bbca11121204d3c78a207ee21f0f9a6d63
MKKL1 <Mkkl3301@gmail.com>
2024-05-26 18:37:02 +0200

    Initial server project

---
+++ server/src/main/java/com/szampchat/server/SzampchatServerApplication.java
@@ -0,0 +1,13 @@
+package com.szampchat.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzampchatServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SzampchatServerApplication.class, args);
+	}
+
+}
