61a94cf0c5a4787b9fbb044999ccc4197c4f538f
Kamil <wisnieibiszkopty@o2.pl>
2024-06-09 14:31:13 +0200

    Created directories structure, started working on signing in

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
+import {LoginComponent} from "./features/pages/login/login.component";
+import {RegisterComponent} from "./features/pages/register/register.component";
+import {AppComponent} from "./app.component";
+import {CommunityComponent} from "./features/pages/community/community.component";
+import {ChannelComponent} from "./features/pages/channel/channel.component";
+import {ProfileComponent} from "./features/pages/profile/profile.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent
+  },
+  {
+    path: 'community',
+    component: CommunityComponent
+  },
+  {
+    path: 'community/channel',
+    component: ChannelComponent
+  }
+];
