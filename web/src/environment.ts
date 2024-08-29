4e90ba3edd3f5f95d779314a6d0d4ace21425643
Kamil <KamilWodowski@o2.pl>
2024-08-29 10:46:50 +0200

    Cors setup

---
+++ web/src/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  // key required to use gif search component
+  // https://developers.giphy.com/
+  giphyKey: 'yourGiphyKey',
+  api: 'http://localhost:8081/api/',
+  keycloackUrl: 'http://localhost:8082'
+}
