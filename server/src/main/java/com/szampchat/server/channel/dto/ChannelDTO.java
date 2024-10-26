f1c3103b6eb4c74623e49b192eef15c7596d269c
MKKL <Mkkl3301@gmail.com>
2024-10-26 12:45:51 +0200

    Add no arg constructor to ChannelDTO

--- server/src/main/java/com/szampchat/server/channel/dto/ChannelDTO.java
+++ server/src/main/java/com/szampchat/server/channel/dto/ChannelDTO.java
@@ -3,9 +3,11 @@ package com.szampchat.server.channel.dto;
 import lombok.AllArgsConstructor;
 import com.szampchat.server.channel.ChannelType;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ChannelDTO {
     private Long id;
     private String name;
