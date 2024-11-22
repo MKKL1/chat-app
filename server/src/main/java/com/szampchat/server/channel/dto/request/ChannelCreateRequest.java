f3810f50e2c8b7f9ed34dd02a32e12dcce5d3bf0
MKKL <Mkkl3301@gmail.com>
2024-11-22 21:53:48 +0100

    Retry connection to rabbitmq

--- server/src/main/java/com/szampchat/server/channel/dto/request/ChannelCreateRequest.java
+++ server/src/main/java/com/szampchat/server/channel/dto/request/ChannelCreateRequest.java
@@ -8,7 +8,7 @@ import lombok.Data;
 @Data
 public class ChannelCreateRequest {
     @NotBlank(message = "Channel name cannot be blank")
-    @Size(max = 64, message = "Channel name cannot exceed 64 characters")
+    @Size(max = 32, message = "Channel name cannot exceed 32 characters")
     private String name;
     private ChannelType type;
 }
