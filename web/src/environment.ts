5d4b589b5debd79c55ea412266a8bf0b93f3ec36
MKKL <Mkkl3301@gmail.com>
2024-11-24 00:10:23 +0100

    Build server image, initial traefik config, get web ready for deployment

--- web/src/environment.ts
+++ web/src/environment.ts
@@ -3,10 +3,10 @@ export const environment = {
   // key required to use gif search component
   // https://developers.giphy.com/
   giphyKey: 'GO6Q2NWnawI61Zdkb11Rv60cijsXPHEw',
-  domain: 'http://localhost:4200/',
-  api: 'http://localhost:8081/api/',
-  keycloackUrl: 'http://localhost:8082',
-  websocketUrl: 'ws://localhost:8083/events',
+  domain: 'http://szampchat.com:4200',
+  api: 'http://szampchat.com/api/',
+  keycloackUrl: 'http://auth.szampchat.com',
+  websocketUrl: 'ws://events.szampchat.com',
   // white2115 birthday
   snowflakeEpoch: 1714867200000
 }
