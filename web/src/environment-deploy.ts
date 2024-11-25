6054231ceeaeb0c8ba8a2d267a273bf75210ae8b
MKKL <Mkkl3301@gmail.com>
2024-11-25 16:45:36 +0100

    Auto import keycloak config

--- web/src/environment-deploy.ts
+++ web/src/environment-deploy.ts
@@ -3,10 +3,10 @@ export const environment = {
   // key required to use gif search component
   // https://developers.giphy.com/
   giphyKey: 'GO6Q2NWnawI61Zdkb11Rv60cijsXPHEw',
-  domain: 'http://localhost:4200/',
-  api: 'http://localhost:8081/api/',
-  keycloackUrl: 'http://localhost:8082',
-  websocketUrl: 'ws://localhost:8083',
+  domain: 'https://szampchat.com',
+  api: 'https://szampchat.com/api/',
+  keycloackUrl: 'https://auth.szampchat.com',
+  websocketUrl: 'wss://events.szampchat.com',
   livekitUrl: 'ws://localhost:7880',
   // white2115 birthday
   snowflakeEpoch: 1714867200000
