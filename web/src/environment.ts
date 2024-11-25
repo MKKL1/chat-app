9aa919bc3f42ab86c4c47d3d4c19c7104df5037d
MKKL <Mkkl3301@gmail.com>
2024-11-25 01:44:16 +0100

    Setup local certificates for https

--- web/src/environment.ts
+++ web/src/environment.ts
@@ -3,10 +3,10 @@ export const environment = {
   // key required to use gif search component
   // https://developers.giphy.com/
   giphyKey: 'GO6Q2NWnawI61Zdkb11Rv60cijsXPHEw',
-  domain: 'http://szampchat.com:4200',
-  api: 'http://szampchat.com/api/',
-  keycloackUrl: 'http://auth.szampchat.com',
-  websocketUrl: 'ws://events.szampchat.com',
+  domain: 'https://szampchat.com',
+  api: 'https://szampchat.com/api/',
+  keycloackUrl: 'https://auth.szampchat.com',
+  websocketUrl: 'wss://events.szampchat.com',
   // white2115 birthday
   snowflakeEpoch: 1714867200000
 }
